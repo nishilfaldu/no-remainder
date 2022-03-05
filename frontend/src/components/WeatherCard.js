0626b9f1dbf2c7dc3ae4e54fed115713bf72de48
Zenith Zack <faldund@outlook.com>
2022-03-05 18:10:45 -0500

    fixed imports

--- frontend/src/components/WeatherCard.js
+++ frontend/src/components/WeatherCard.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './WeatherCard.css'
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
 
 function WeatherCard() {
   return (
     <div className="WeatherCard">
       <h1>Card Title</h1>
       <h2 className = "temp">52</h2>
-
+      <WbSunnyIcon></WbSunnyIcon>
     </div>
   )
 }
