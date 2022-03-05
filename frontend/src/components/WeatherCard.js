e92dadf93f249bf280070b28d828a60bab53d842
luketheduke112 <lukedavidfisher03@gmail.com>
2022-03-05 18:05:42 -0500

    added weather card function

---
+++ frontend/src/components/WeatherCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './WeatherCard.css'
+
+function WeatherCard() {
+  return (
+    <div className="WeatherCard">
+      <h1>Card Title</h1>
+      <h2 className = "temp">52</h2>
+
+    </div>
+  )
+}
+
+export default WeatherCard;
