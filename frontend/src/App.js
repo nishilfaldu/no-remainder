7495101652394dde9864b914d466031ba2b07664
Zenith Zack <faldund@outlook.com>
2022-03-05 19:19:44 -0500

    weather api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import React from "react";
 import ServerCard from './components/ServerCard';
-
+import WeatherCard from './components/WeatherCard';
 
 function App() {
   return (
     <div className="App">
+      <WeatherCard></WeatherCard>
       <ServerCard></ServerCard>
     </div>
   );
---
+++ frontend/src/components/WeatherCard.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './WeatherCard.css'
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import { useState, useEffect } from 'react'
+
+function WeatherCard() {
+
+    const [weather, setWeather] = useState(null);
+
+    useEffect(() => {
+        fetch('http://api.weatherapi.com/v1/current.json?key=080c2802be9e48b8ae3233324220503&q=Cincinnati&aqi=no')
+            .then(response => response.json())
+            .then(data => {
+                setWeather(Math.round(data.current.temp_f));
+            })
+    })
+
+  return (
+    <div className="WeatherCard">
+      <h1>Weather</h1>
+      <div className="temp">
+        <h2>{weather}°</h2>
+      </div>
+      <div className="ico">
+        <WbSunnyIcon sx={{fontSize:40}}></WbSunnyIcon>
+      </div>
+
+      <h5>Cincinnati, Ohio</h5>
+    </div>
+  )
+}
+
+export default WeatherCard;
